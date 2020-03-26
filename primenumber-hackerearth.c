74ee0c14cab0d206cf1dfa5114ef350c83f8759a
Arvind_Suresh <53540697+Arvind0515@users.noreply.github.com>
2020-03-26 15:00:37 +0530

    Add files via upload

---
+++ primenumber-hackerearth.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+ 
+void main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    
+    for(i=2;i<=n;i++)
+    {
+        int c=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                c++;
+            }
+        }
+         
+       if(c==2)
+        {
+            printf("%d ",i);
+        }
+    }
+return 0;
+}
