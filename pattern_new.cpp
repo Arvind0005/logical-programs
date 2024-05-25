cfef74c907e84554aa74dc3d86adbbe7894df21f
Arvind_Suresh <53540697+Arvind0005@users.noreply.github.com>
2024-05-25 12:53:59 +0530

    zoho patterns example
    zoho patterns example

---
+++ pattern_new.cpp
@@ -0,0 +1,211 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+//	int n=5;
+//	for(int i=0;i<n;i++)
+//	{
+//		for(int j=0;j<=i;j++)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<"\n";
+//	}
+//	int k=3;
+//	for(int i=0;i<n-1;i++)
+//	{
+//		for(int j=0;j<n-1;j++)
+//		{
+//			if(j<=k)
+//				cout<<"*";
+//			else
+//				cout<<" ";
+//		}
+//		k--;
+//		cout<<"\n";
+//	}
+	//op:
+//*
+//**
+//***
+//****
+//*****
+//****
+//***
+//**
+//*
+
+//	int n=5;
+//	for(int i=0;i<n;i++)
+//	{
+//		for(int j=0;j<n;j++)
+//		{
+//			if(n-j-2>=i)
+//				cout<<" ";
+//			else
+//				cout<<"*";
+//		}
+//		cout<<"\n";
+//	}
+//	for(int i=1;i<n;i++)
+//	{
+//		for(int j=0;j<n;j++)
+//		{
+//			if(j>=i)
+//				cout<<"*";
+//			else
+//				cout<<" ";
+//		}
+//		cout<<"\n";
+//	}
+//  *
+//   **
+//  ***
+// ****
+//*****
+// ****
+//  ***
+//   **
+//    *
+//	int n=5;
+//	for(int i=0;i<n;i++)
+//	{
+//		for(int j=0;j<n;j++)
+//		{
+//			if(n-1-j<=i)
+//				cout<<"* ";
+//			else
+//				cout<<" ";
+//		}
+//		cout<<"\n";
+//	}
+//	for(int i=1;i<n;i++)
+//	{
+//		for(int k=1;k<=i;k++)
+//			cout<<" ";
+//		for(int j=0;j<n;j++)
+//		{
+//			if(n-1-j>=i)
+//				cout<<"* ";
+//			else
+//				cout<<" ";
+//		}
+//		cout<<"\n";
+//	}
+//    *
+//   * *
+//  * * *
+// * * * *
+//* * * * *
+// * * * *
+//  * * *
+//   * *
+//    *
+
+//	int n=5;
+//	for(int i=0;i<n;i++)
+//	{
+//		for(int k=0;k<=i;k++)
+//			cout<<" ";
+//		for(int j=0;j<n;j++)
+//		{
+//			if(n-1-j>=i)
+//				cout<<"* ";
+//			else
+//				cout<<" ";
+//		}
+//		cout<<"\n";
+//	}
+//	for(int i=1;i<n;i++)
+//	{
+//		cout<<" ";
+//		for(int j=0;j<n;j++)
+//		{
+//			if(n-1-j<=i)
+//				cout<<"* ";
+//			else
+//				cout<<" ";
+//		}
+//		cout<<"\n";
+//	}
+// * * * * *
+//  * * * *
+//   * * *
+//    * *
+//     *
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+
+//	int n=5;
+//	int num=1;
+//	for(int i=0;i<n;i++)
+//	{
+//		for(int j=0;j<n;j++)
+//		{
+//			if(j<=i)
+//			{
+//				cout<<num;
+//				num++;
+//			}
+//			else
+//				cout<<" ";
+//				
+//		}
+//		cout<<"\n";
+//	}
+//1
+//23
+//456
+//78910
+
+//	int n=5;
+//	int num;
+//	for(int i=0;i<n;i++)
+//	{
+//		num=i+1;
+//		for(int j=0;j<n;j++)
+//		{
+//			if(n-1-j<=i)
+//			{
+//				cout<<num;
+//				num++;
+//			}
+//			else
+//				cout<<" ";
+//		}
+//		cout<<"\n";
+//	}
+//    1
+//   23
+//  345
+// 4567
+//56789
+//	int n=5;
+//	for(int i=0;i<n;i++)
+//	{
+//		int num=i+1;
+//		for(int j=0;j<n;j++)
+//		{
+//			if(n-1-j<=i)
+//			{
+//				cout<<num;
+//				num++;
+//			}
+//			else
+//				cout<<" ";
+//		}
+//		if(i!=0)
+//		{
+//			for(int k=num-2;k>=i+1;k--)
+//				cout<<k;
+//		}
+//		cout<<"\n";
+//	}
+//	  1
+//   232
+//  34543
+// 4567654
+//567898765
+}
